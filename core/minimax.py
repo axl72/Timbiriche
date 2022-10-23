f04add98b7fdcfff9af285528873cce991372ca9
axl72 <axell.bernabel@unmsm.edu.pe>
2022-10-22 21:17:39 -0500

    se agrego script minimax - no está terminado

---
+++ core/minimax.py
@@ -0,0 +1,20 @@
+from table import Table
+
+tablero = Table(7, 7)
+
+def pivotear_estado(estado):
+    pivote = 3 if estado else 3
+
+def generar_estados(estado: Table):
+    result = []
+    
+    for row in estado.
+
+    return
+
+def funcion_minimax(estado):
+    pass
+
+
+if __name__ == "__main__":
+    pass
